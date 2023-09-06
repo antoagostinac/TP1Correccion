75ddbde9c4fefcfece14674ee478065626d03c36
Azul144 <134295386+Azul144@users.noreply.github.com>
2023-09-06 13:10:06 -0300

    claseEdificio.py

--- claseEdificio.py
+++ claseEdificio.py
@@ -1,12 +1,14 @@
 #Azul
-#explicar clase abstracta por que
 class Edificio():
-    #explicar constructor, definir por que los atributos son privados
+#Se crea una clase edificio
     def __init__(self, nombre, cantidadPisos, direccion):
+#El constructor toma tres argumentos nombre, cantidadPisos y direccion.
+#Los atributo se definden como privado ya que solo pueden acceder desde dentro de la clase. 
+#Estos argumentos se utiliza para inicializar los atributod del objeto __nombre,__cantidadPisos y __direccion. 
         self.__nombre = nombre
         self.__cantidadPisos = cantidadPisos
         self.__direccion = direccion
-    #explicar get
+#Esto es un metodo get para la clase edificio se va utilizar para recuperar el valor de un atributo que seria name.
     def get(self, name):
         if(name=="nombre"):
             return self.__nombre
@@ -14,15 +16,17 @@ class Edificio():
             return self.__cantidadPisos
         elif(name=="direccion"):
             return self.__direccion
-    #explicar set
     def set(self, name, valor):
+#Esto es metodo set para la clase edificio se utiliza para estabalecer el valor de un atributo.
+#El metodo toma dos argumento name que especifica el nombre del atributo a establacer y valor que especifica el nuevo valor para el atributo.
         if(name=="nombre"):
             self.__nombre = valor
         elif(name=="cantidadPisos"):
             self.__cantidadPisos = valor
         elif(name=="direccion"):
             self.__direccion = valor
-    #explicar por que metodo publico
+#Esto es un metodo publico llamado mostrarInformacio para la clase edificio .
+#Un metodo publico puede ser accedido desde fuera de la clase y el metodo lo devuelve una cadena que contine infromacion sobre los atributos.
     def mostrarInformacion(self):
         return f'Nombre: {self.__nombre}\nCantidad de pisos: {self.__cantidadPisos}\nDireccion: {self.__direccion}'
 
